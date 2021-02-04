ee1fac57f9697f920a3346fae3d4df9a7b8a1375
Marsocode <53379742+Marsocode@users.noreply.github.com>
2021-02-04 07:20:10 +0300

    Delete Carcassonne directory

--- Carcassonne/data/gameCards.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/extensions */
-import { objectGenerator } from '../utils/objectGenerator.js';
-import shuffle from '../utils/shuffle.js';
-
-const CARDS = shuffle(shuffle(shuffle(objectGenerator())));
-
-export default CARDS;
